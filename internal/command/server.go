af99292aed84732d23d4978eb1bb11fd59927ba3
Shu Kutsuzawa <cappyzawa@yahoo.ne.jp>
2021-02-23 00:15:45 +0900

    enable to overwrite property by env values

--- internal/command/server.go
+++ internal/command/server.go
@@ -1,6 +1,8 @@
 package command
 
 import (
+	"os"
+
 	"github.com/cappyzawa/terraform-registry/internal/server"
 	"github.com/spf13/cobra"
 )
@@ -16,6 +18,7 @@ func NewServerCmd() *cobra.Command {
 		Use:   "server",
 		Short: "run terraform registry locally",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			overWriteByEnv()
 			so := &server.Opt{
 				Port:       port,
 				ConfigPATH: configFile,
@@ -29,4 +32,16 @@ func NewServerCmd() *cobra.Command {
 	cmd.PersistentFlags().StringVarP(&configFile, "config", "c", "config.yaml", "specify config file for registry")
 	cmd.PersistentFlags().StringVarP(&pidFile, "pid-file", "", "", "write PID of registy to specified file")
 	return cmd
+}
+
+func overWriteByEnv() {
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+	if os.Getenv("CONFIG_FILE") != "" {
+		configFile = os.Getenv("CONFIG_FILE")
+	}
+	if os.Getenv("PID_FILE") != "" {
+		pidFile = os.Getenv("PID_FILE")
+	}
 }
